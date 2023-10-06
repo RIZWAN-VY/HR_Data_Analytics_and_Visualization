cf9d33b6b93085339f447f2910ec9bf9665bb05f
rizwanyoosaf37@gmail.com <rizwanyoosaf37@gmail.com>
2023-10-06 10:34:43 +0530

    created a function for salary category column

--- Employee_Data_Analysis.py
+++ Employee_Data_Analysis.py
@@ -10,3 +10,24 @@ df = pd.read_csv('C:/Users/rizwa/Desktop/Rizwan/Projects/HR_Data_Analysis_and_Vi
 print(df.head())    # Display the first few rows of the DataFrame
 
 print(df.info())    # Display data types and missing values
+
+#---------------------------------------------------------------------------------------------------
+
+# Creating additional columns 
+
+# SALARY CATEGORY :
+
+# Define Salary Category
+def assign_Salary_Category(salary):
+    if salary <= 200000:
+        return "Less than 2 LPA"
+    elif salary <= 400000:
+        return "2LPA - 4LPA"
+    elif salary <= 600000:
+        return "4LPA - 6LPA"
+    elif salary <= 800000:
+        return "6LPA - 8LPA"
+    elif salary <= 1000000:
+        return "8LPA - 10LPA"
+    else:
+        return "Greater than 10 LPA "
