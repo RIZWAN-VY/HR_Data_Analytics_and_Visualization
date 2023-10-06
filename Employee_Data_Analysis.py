f361e9d2122a852c761a7a2845d846b83c8f5467
rizwanyoosaf37@gmail.com <rizwanyoosaf37@gmail.com>
2023-10-06 10:27:22 +0530

    Loaded the dataset

---
+++ Employee_Data_Analysis.py
@@ -0,0 +1,11 @@
+#    HR DATA - EMPLOYEE DATA ANALYTICS
+
+# Libraries 
+import pandas as pd
+
+# Loading the data and reading the CSV file
+
+df = pd.read_csv('C:/Users/rizwa/Desktop/Rizwan/Projects/HR_Data_Analysis_and_Visualization/HR_Employee_data_kaggle.csv')
+
+print(df.head())    # Display the first few rows of the DataFrame
+
